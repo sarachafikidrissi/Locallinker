0471a4dc99863ceee33d32cc486c687cd8caf1d1
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-02-26 22:58:16 +0100

    sign-in/register

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 
 views = Blueprint(__name__, "home")
 
@@ -6,3 +6,11 @@ views = Blueprint(__name__, "home")
 @views.route('/home')
 def home():
     return render_template('index.html')
+
+@views.route('/register')
+def register():
+    return render_template('register.html')
+
+@views.route('/login')
+def login():
+    return render_template('login.html')
