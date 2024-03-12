fb42054d1babfd43ee544c142e8f8d66c228d176
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-03-12 17:10:23 +0000

    update run file

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from application import app
+import os
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        app.run(host="0.0.0.0", port=5000)
+    # with app.app_context():
+    #     app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
